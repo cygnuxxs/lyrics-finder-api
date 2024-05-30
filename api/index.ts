bbdf00f371faa98ded5b2946b97aeb0e933b99e0
Ashok <56536970+cygnuxxs@users.noreply.github.com>
2024-05-31 01:31:10 +0530

    migrate to vercel

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+import express, { Request, Response } from 'express';
+import cors from 'cors'
+import Genius from 'genius-lyrics';
+
 const app = express();
 const port = process.env.PORT || 1155;
-const Genius = require('genius-lyrics');
 const Client = new Genius.Client();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/search', async (req, res) => {
-    const { query } = req.query;
+app.get('/search', async (req: Request, res: Response) => {
+    const query: string | undefined = req.query.query as string;
     if (!query) {
         return res.status(400).send('Query parameter is required.');
     }
     try {
         const response = await Client.songs.search(query);
-        const songs = response.map(song => ({
+        const songs = response.map((song: any) => ({
             songId: song.id,
             title: song.title,
             fullTitle: song.fullTitle,
             image: song.image,
             artistName: song.artist?.name,
             albumName: song.album?.name,
-            url : song.url,
+            url: song.url,
             releaseDate: song._raw?.release_date_with_abbreviated_month_for_display
         }));
         res.json(songs);
@@ -32,14 +33,21 @@ app.get('/search', async (req, res) => {
     }
 });
 
-app.post('/lyrics', async (req, res) => {
-    const {url} = req.body
+app.post('/lyrics', async (req: Request, res: Response) => {
+    const { url } = req.body;
     if (url) {
-        const ScrapedSong = await Client.songs.scrape(url)
-        const lyrics = await ScrapedSong.lyrics(true)
-        res.json(lyrics)
-    } else res.status(400).send({message : 'URL not provided'})
-})
+        try {
+            const ScrapedSong = await Client.songs.scrape(url);
+            const lyrics = await ScrapedSong.lyrics(true);
+            res.json(lyrics);
+        } catch (error) {
+            console.error('Error fetching lyrics:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    } else {
+        res.status(400).send({ message: 'URL not provided' });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
